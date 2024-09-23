dfb7f0ad2f6c6885f3c1a19fec754812aace8aba
Sankaranarayanankm <sankaranarayanan.km97@gmail.com>
2024-09-23 12:38:11 +0530

    added search page

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./SearchPage.css";
+import { Button } from "@mui/material";
 
 const SearchPage = () => {
-  return <div className="searchPage">SearchPage</div>;
+  return (
+    <div className="searchPage">
+      <h1>I'm Search Page</h1>
+      <div className="searchPage__info">
+        <p>62 stays . 26 augest to 30 august. 2 guests</p>
+        <h1>Stays Nearby</h1>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Price</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">More filters</Button>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
